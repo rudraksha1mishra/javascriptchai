5527d9839243fab935b38a93b13bb8d7c698a4fc
Rudraksha Mishra <80878358+rudraksha1mishra@users.noreply.github.com>
2024-09-17 17:38:48 +0000

    sting

---
+++ 01_basics/comparison.js
@@ -0,0 +1,14 @@
+const my_function = function(){
+    console.log("hm");
+}
+const id = {
+    name : "rudra",
+    age : 22
+}
+//const arr = [1,1,2,3];
+//console.log(typeof(my_function));
+//console.log(typeof(id));
+//console.log(typeof(arr));
+const call = "rudraa";
+console.log(call[1]);
+
